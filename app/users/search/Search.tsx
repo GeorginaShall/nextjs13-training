c998b1cc5fe6371b25cff734ac89b2cc02da1e9c
gugushall <107054293+gugushall@users.noreply.github.com>
2023-01-27 02:35:28 +0200

    new

--- app/users/search/Search.tsx
+++ app/users/search/Search.tsx
@@ -11,7 +11,7 @@ function Search() {
     const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setSearch("");
-        router.push('/search/'+ search)
+        router.push('/users/search/'+ search)
     };
 
 
